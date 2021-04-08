9156548703577c37e001aa0c8fc450c182826684
Marie <marie.marcos25@gmail.com>
2021-04-08 12:58:21 -0700

    work on search and map feature

---
+++ src/CitySearch.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+
+class CitySearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.handleSearch(this.state.value);
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({ value: event.target.value })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Enter a City Location:</label>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Explore!" />
+      </form>
+
+    );
+  }
+}
+
+export default CitySearch;
